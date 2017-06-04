db541a744540396ab0a3dec06360ded9ebf54c43
Armeen Mahdian <mahdianarmeen@gmail.com>
2017-06-04 08:40:58 -0500

    Actually commit changes of JS script movement (odd git issue?).

--- header.js
+++
@@ -1,3 +0,0 @@
-document.write('<link href="https://fonts.googleapis.com/css?family=Khula:400,600" rel="stylesheet"><link rel="stylesheet" href="websitecss.css" type="text/css">');
-
-document.write('<div class="header"><div class="nav"><ul><img class="logo" src="yellow_transparent_sandcrawler.png" alt="Logo"><li><a href="index.html">Home</a></li><li><a href="team.html">About</a></li><li><a href="https://drive.google.com/open?id=0BykwGzOiMu2Db0tGQ1pkcmNWdm8" target="blank">Media</a></li><li><a href="schedule.html">Schedule</a></li><li><a href="parents.html">Parents</a></li><li><a href="contact.html">Contact</a></li><img class="first" src="http://team4element.com/wp-content/uploads/2017/01/FIRST_vert.gif" alt="FIRST"></ul></div></div>');
