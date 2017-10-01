a2d835a7eb7298a594ea41dceda7cf3e4a3138ba
andenrx <andenrx@gmail.com>
2017-10-01 17:34:17 -0500

    Create head.php

---
+++ Root/head.php
@@ -0,0 +1,11 @@
+<?php echo'
+<head>
+	<title>Jawas - Team 1984</title>
+	<meta name="description" content="The Jawas, Team 1984, is a FIRST robotics team from Overland Park, KS.">
+	<link href="https://fonts.googleapis.com/css?family=Khula:400,600" rel="stylesheet">
+	<link rel="stylesheet" href="CSS/websitecss.css" type="text/css">
+	<meta name="viewport" content="width=device-width">
+	<link rel="shortcut icon" href="Media/favicon.ico" type="image/x-icon">
+	<script src="JS/equalheight.js"></script>
+</head>
+';?>
