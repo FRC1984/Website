aa8624329229106a8a233ea3b03fb62810052748
madelinebermond <madelinebermond@gmail.com>
2017-12-13 16:02:54 -0600

    seo

--- Root/head.php
+++ Root/head.php
@@ -1,5 +1,6 @@
 <?php echo'
-	<title>Jawas - Team 1984</title>
+	<title>FRC Team 1984-Jawas</title>
+    <meta name="keywords" content="1984, jawas, first, frc, robot, robotics, robots, team"/>
 	<meta name="description" content="The Jawas, Team 1984, is a FIRST robotics team from Overland Park, KS.">
 	<link href="https://fonts.googleapis.com/css?family=Khula:400,600" rel="stylesheet">
 	<link rel="stylesheet" href="CSS/websitecss.css" type="text/css">
