144fb484e510959ed134297391e6446c19fec9ab
Armeen Mahdian <mahdianarmeen@gmail.com>
2018-01-14 12:09:05 -0600

    More link fixing, added stuff from other branch...

---
+++ awards.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include 'head.php';?>
+	</head>
+
+	<body id="awards">
+        
+        <?php include 'header.php';?>
+		
+		<div class="l-wrap">
+			<div class="main-grid">
+
+				<div class="grid-item l-6">
+					<div class="grid-inner">
+						<div class="heading">
+							<h1>Awards and Achievements</h1>
+						</div>
+							<h2>Ozark Mountain Brawl 2016<br>
+                        Second Place</h2>
+							<img class="award" src="Media/2016image_40.jpg" alt="Second Place">
+					</div>
+				</div>
+                
+                <?php include 'sidebar.php';?>
+                
+			</div>
+		</div>
+		
+        
+		<?php include 'footer.php';?>
+
+	</body>
+</html>
---
+++ schedule.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include 'head.php';?>
+	</head>
+
+	<body id="schedule">
+        
+        <?php include 'header.php';?>
+    
+		<div class="l-wrap">
+			<div class="main-grid">
+ 
+				<div class="grid-item l-6">
+					<div class="grid-inner">
+						<div class="heading">
+							<h1>Schedule</h1>
+						</div>
+						<div class="calendar">
+							<iframe src="https://calendar.google.com/calendar/embed?src=1984robotics%40gmail.com&ctz=America/Chicago" ></iframe>
+						</div>
+					</div>
+				</div>
+                
+                <?php include 'sidebar.php';?>
+                
+			</div>
+		</div>
+
+        
+		<?php include 'footer.php';?>
+
+	</body>
+</html>
